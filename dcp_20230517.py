8e5b44ac7e59c68ff2d99603c6e79bc71797a0e1
Tianna <75389500+UselessToaster@users.noreply.github.com>
2023-05-17 17:41:37 -0500

    Update dcp_20230517.py
    I was encouraged to use more proper python notation so I edited some of my variable names.
    I also switched the order of a and c in the for loops for more efficiency

--- dcp_20230517.py
+++ dcp_20230517.py
@@ -10,43 +10,42 @@
 #Thoughts: 
 # - if statement to determine pythagorean triplet
 # - must itterate through every number in every position
-# - remember a and b individually are always less than c
-# however a + b must always be greater than c
+~~# - remember a and b individually are always less than c~~
+~~# however a + b must always be greater than c~~
 
 import random 
 
 def main():
     #test array
-    user = int(input("How long do you want your array to be? "))
-    arr = randArr(user)
+    arr_len = int(input("How long do you want your array to be? "))
+    arr = rand_arr(arr_len)
     
     print(f"Given list: {arr}")
 
-    sansDuplicate = [] #store possible duplicate triples
+    calc_results = [] 
 
     print("Pythagorean triples in the given list:")
 
-    noTriples = True 
-    for a in arr:
+    no_triples = True 
+    for c in arr:
         for b in arr:
-            for c in arr:
+            for a in arr:
                 if (a**2 + b**2 == c**2):
-                    sansDuplicate.append(c)
-                    noTriples = False
+                    calc_results.append(c)
+                    no_triples = False
 
                     #only prints first iteration of triple
-                    if sansDuplicate.count(c) == 1:
+                    if calc_results.count(c) == 1:
                         print(f"({a}, {b}, {c})")                 
 
-    if noTriples == True:
+    if no_triples == True:
         print("There were no Pythagorean triplets in this list.")
 
 
-def randArr(length):
+def rand_arr(length):
     box = []
     for i in range(length):
         box.append(random.randint(1,100))
-
     return box
 
 main()
