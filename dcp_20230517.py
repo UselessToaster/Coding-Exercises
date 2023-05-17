ddbd4d37c7229e4638e6a78c2a6586cea4b3d471
Tianna <75389500+UselessToaster@users.noreply.github.com>
2023-05-17 18:41:23 -0500

    Update2 dcp_20230517.py
    I separated the pythagorean triple checker and the display code into their respective functions for simplicity

--- dcp_20230517.py
+++ dcp_20230517.py
@@ -1,7 +1,5 @@
-#**EASY**#
+#Daily Coding Problem: EASY
 #Good morning! Heres your coding interview problem for today.  
-
-#This problem was asked by Netflix.
 
 #Given an array of integers, determine whether it contains a Pythagorean triplet. 
 #Recall that a Pythogorean triplet (a, b, c) is defined by the equation a2+ b2= c2.
@@ -10,36 +8,17 @@
 #Thoughts: 
 # - if statement to determine pythagorean triplet
 # - must itterate through every number in every position
-~~# - remember a and b individually are always less than c~~
-~~# however a + b must always be greater than c~~
 
 import random 
 
 def main():
     #test array
     arr_len = int(input("How long do you want your array to be? "))
-    arr = rand_arr(arr_len)
+    arr = list(set(rand_arr(arr_len)))
     
     print(f"Given list: {arr}")
 
-    calc_results = [] 
-
-    print("Pythagorean triples in the given list:")
-
-    no_triples = True 
-    for c in arr:
-        for b in arr:
-            for a in arr:
-                if (a**2 + b**2 == c**2):
-                    calc_results.append(c)
-                    no_triples = False
-
-                    #only prints first iteration of triple
-                    if calc_results.count(c) == 1:
-                        print(f"({a}, {b}, {c})")                 
-
-    if no_triples == True:
-        print("There were no Pythagorean triplets in this list.")
+    display(pythag_checker(arr))
 
 
 def rand_arr(length):
@@ -47,5 +26,27 @@ def rand_arr(length):
     for i in range(length):
         box.append(random.randint(1,100))
     return box
+
+
+def pythag_checker(pc_arr):
+    triples_matrix = []
+
+    for c in pc_arr:
+        triples = []
+        for a in pc_arr:
+            for b in pc_arr:
+                if (a**2 + b**2 == c**2):
+                    triples = [a, b, c]
+
+                    #only notates first iteration of triple
+                    if sorted(triples) not in triples_matrix:
+                        triples_matrix.append(triples)
+    return triples_matrix
+
+
+def display(d_triples_matrix):
+    print(f"There are {len(d_triples_matrix)} Pythagorean triples in the given list.")
+    for i in d_triples_matrix:
+        print(i)   
 
 main()
